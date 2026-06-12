74183d7350d8d8ed64f62d1a97270db462f30755
Arjun santhosh <124417632+Arjunsanthosh09@users.noreply.github.com>
2026-06-12 18:27:03 +0530

    Update guicalculator.java

--- Largeprograms/Largeprograms/guicalculator.java
+++ Largeprograms/Largeprograms/guicalculator.java
@@ -1,93 +1,77 @@
-
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
-public class guicalculator extends JFrame implements ActionListener{
-    JLabel l1,l2,l3;
-    JButton add,sub,div,mult;
-    JTextField t1,t2,result;
-    public guicalculator() {
-    setSize(600,600);
-    setTitle("Calculator");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    setLayout(null);  
-    l1 = new JLabel("Enter First number:");
-    l1.setBounds(50, 100, 150, 30);
-    add(l1);
-    
-    t1=new JTextField();
-    t1.setBounds(200, 100, 150, 30);
-    add(t1);
-
-    l2 = new JLabel("Enter Second number:");
-    l2.setBounds(50, 200, 150, 30);
-    add(l2);
-    
-    t2=new JTextField();
-    t2.setBounds(200, 200, 150, 30);
-    add(t2);
-    
-    add=new JButton("add");
-    sub=new JButton("Substract");
-    div=new JButton("Division");
-    mult=new JButton("Multiplication");
-    
-    add.setBounds(50, 300, 100, 30);
-    sub.setBounds(160, 300, 100, 30);
-    div.setBounds(280, 300, 100, 30);
-    mult.setBounds(390, 300, 150, 30);
-    
-    add(add);
-    add(sub);
-    add(div);
-    add(mult);
-    
-    l3=new JLabel("Result: ");
-    l3.setBounds(50, 450, 150, 30);
-    add(l3);
-    result=new JTextField();
-    result.setBounds(150, 450, 200, 30);
-    add(result);
-    
-    add.addActionListener(this);
-    sub.addActionListener(this);
-    div.addActionListener(this);
-    mult.addActionListener(this);
-    
-    setVisible(true);
+public class GuiCalculator extends JFrame implements ActionListener {
+    JTextField display;
+    JButton[] buttons;
+    String[] text = {
+        "7", "8", "9", "/",
+        "4", "5", "6", "*",
+        "1", "2", "3", "-",
+        "0", "C", "=", "+"
+    };
+    double num1, num2, result;
+    String operator;
+    public GuiCalculator() {
+        setTitle("Calculator");
+        setSize(350, 450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        display = new JTextField();
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+        JPanel panel = new JPanel(new GridLayout(4, 4, 5, 5));
+        buttons = new JButton[text.length];
+        for (int i = 0; i < text.length; i++) {
+            buttons[i] = new JButton(text[i]);
+        }
+        for (JButton btn : buttons) {
+            btn.addActionListener(this);
+            panel.add(btn);
+        }
+        add(panel, BorderLayout.CENTER);
+        setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        try{
-            double num1=Double.parseDouble(t1.getText());
-            double num2=Double.parseDouble(t2.getText());
-            double res;
-            if(e.getSource()==add){
-                res=num1+num2;
-                result.setText(String.valueOf(res));
-            }else if(e.getSource()==sub){
-                res=num1-num2;
-                result.setText(String.valueOf(res));
-            }else if(e.getSource()==div){
-                try{
-                    res=num1/num2;
-                    result.setText(String.valueOf(res));
-                }catch(ArithmeticException a){
-                    System.out.println(e);
-                    System.out.println("Cannot divided by zero...");
-                }
-                
+        String cmd = e.getActionCommand();
+        if (cmd.matches("[0-9]")) {
+            display.setText(display.getText() + cmd);
+        }
+        else if (cmd.matches("[+\\-*/]")) {
+            num1 = Double.parseDouble(display.getText());
+            operator = cmd;
+            display.setText("");
+        }
+        else if (cmd.equals("=")) {
+            num2 = Double.parseDouble(display.getText());
+            switch (operator) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        display.setText("Cannot divide by zero");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
             }
-            else if(e.getSource()==mult){
-                res=num1*num2;
-                result.setText(String.valueOf(res));
-            }
-    
-        }catch(NumberFormatException n){
-            System.out.println(n);
+            display.setText(String.valueOf(result));
+        }
+        else if (cmd.equals("C")) {
+            display.setText("");
+            num1 = num2 = result = 0;
+            operator = "";
         }
     }
-    public static void main(String []arg){
-        guicalculator g1=new guicalculator();
+    public static void main(String[] args) {
+        new GuiCalculator();
     }
 }
