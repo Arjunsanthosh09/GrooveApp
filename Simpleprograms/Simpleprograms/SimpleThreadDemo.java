f30a3913b39bbfa08b144c7e4e69b107ed48ce5b
Arjun santhosh <124417632+Arjunsanthosh09@users.noreply.github.com>
2026-06-12 06:32:35 +0530

    Update SimpleThreadDemo.java

--- Simpleprograms/Simpleprograms/SimpleThreadDemo.java
+++ Simpleprograms/Simpleprograms/SimpleThreadDemo.java
@@ -1,26 +1,38 @@
-package com.mycompany.javalabcode;
 
-class Thread1 extends Thread {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Thread 1");
+package com.mycompany.practice;
+
+class oddthread extends Thread{
+    public void run(){
+        try{
+           for(int i=1;i<=10;i++){
+            if(i%2==1){
+                System.out.println(i+" is odd ");
+                Thread.sleep(1000);
+            }  
+        }
+ 
+        }catch(Exception e){
+            System.out.println(e);
+        }
+     }
+}
+class eventhread extends Thread{
+    public void run(){
+        try{
+            for(int i=1;i<=10;i++){
+                if(i%2==0){
+                    System.out.println(i+" is even");
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
         }
     }
 }
-
-class Thread2 extends Thread {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Thread 2");
-        }
-    }
-}
-
-public class SimpleThreadDemo {
-    public static void main(String[] args) {
-        Thread1 t1 = new Thread1();
-        Thread2 t2 = new Thread2();
-
+public class threaddemo {
+    public static void main(String []arg){
+        oddthread t1=new oddthread();
+        eventhread t2=new eventhread();
         t1.start();
         t2.start();
     }
